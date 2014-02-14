4397831be1e3002920232b372fbc677fb07b4f43
revenyoung <revenyoung@revenyoungdeMacBook-Pro.local>
2014-02-14 20:19:23 +0800

    Initial Commit

---
+++ iTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTest
+//
+//  Created by revenyoung on 14-2-14.
+//  Copyright (c) 2014年 revenyoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTestTests/iTestTests.m
@@ -0,0 +1,34 @@
+//
+//  iTestTests.m
+//  iTestTests
+//
+//  Created by revenyoung on 14-2-14.
+//  Copyright (c) 2014年 revenyoung. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iTestTests : XCTestCase
+
+@end
+
+@implementation iTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
